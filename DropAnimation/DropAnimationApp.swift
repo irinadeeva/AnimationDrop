648f8046388479020e35f913e1260a2878f3a7d3
irinadeeva <deeva97@me.com>
2024-12-25 15:15:37 +0300

    Initial Commit

---
+++ DropAnimation/DropAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropAnimationApp.swift
+//  DropAnimation
+//
+//  Created by Irina Deeva on 25/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DropAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
